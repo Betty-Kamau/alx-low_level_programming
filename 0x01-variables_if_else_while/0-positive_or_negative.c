bec5e9dd5689d62a42a7a7bb3421ae153fc03bd1
root <root@LAPTOP-J0331KVR.localdomain>
2022-03-11 17:33:49 +0300

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,6 @@
  *The program decides whether a random number is positive, negative or zero 
  *Return: Always 0 (Success)
  */
-
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -17,15 +16,15 @@ n = rand() - RAND_MAX / 2;
 /*your code goes there*/
 if (n > 0)
 {
-   printf("%d is positive\n", n);
+	printf("%d is positive\n", n);
 }
 else if (n <  0)
 {
-   printf("%d is negative\n", n);
+	printf("%d is negative\n", n);
 }
 else
 {
-   printf("%d is zero\n", n);
+	printf("%d is zero\n", n);
 }
 return (0);
 }
