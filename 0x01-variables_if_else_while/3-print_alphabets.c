b439ea610b5d034f38e00a858944be73ca578ac3
root <root@LAPTOP-J0331KVR.localdomain>
2022-03-12 17:29:03 +0300

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *The function prints all lowercase numbers
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	        char ch = 'a';
+
+		while (ch <= 'z')
+                {
+			putchar(ch);
+			ch++;
+		}
+		char CH = 'A';
+
+		while (ch <= 'Z')
+		{
+			putchar(CH);
+			CH++;
+		}
+	 	putchar('\n');
+		return (0);
+}
