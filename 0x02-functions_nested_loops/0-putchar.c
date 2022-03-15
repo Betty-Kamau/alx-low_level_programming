a7ac8e405135adebf67eac957da979c935af55ad
root <root@LAPTOP-J0331KVR.localdomain>
2022-03-15 15:17:38 +0300

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * The program prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	char p[] = '_putchar';
+
+	while <= 7;
+	{
+		putchar(p[i]);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+A README FILE FOR THE PROJECT IN THE 0x02-functions_nested_loops DIRECTORY
+1. The first file, 0-putchar.c contains a program that will _putchar, followed by a new line
