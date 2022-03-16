281772ad849a9c88d0a2ad5a786dfffbf39c7b7f
root <root@LAPTOP-J0331KVR.localdomain>
2022-03-16 19:18:26 +0300

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,23 +11,13 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 /*
-<<<<<<< HEAD
 *	i = 0;
 *
 *	while (i < 10)
 *	{
 *		putchar(i);
-*	}
-*/	
-=======
- *	i = 0;
- *	
- *	while (i < 10)
- *	{
- *		putchar(i);
- *	}
+*	}	
 */
->>>>>>> 2e98a1a2ea1a703b5349549bbec7615b3043352b
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
