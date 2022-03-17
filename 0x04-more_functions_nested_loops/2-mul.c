d01e45ecf728c98799948f99a96b8b16ebb3f85a
LMI-crypto <bettykamau333@gmail.com>
2022-03-17 20:49:23 +0300

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,14 @@
 #include "main.h"
+
 /**
  * mul - multiplies two intergers
  * Return: Always 0 (Success)
- * */
+ */
 
 int mul(int a, int b)
 {
 	int c;
 	c = a * b;
 	return (c);
+	putchar('\n')
 }
