bdce45bc8e9390d118bb8285144711795b1fe069
LMI-crypto <bettykamau333@gmail.com>
2022-03-17 20:42:05 +0300

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two intergers
+ * Return: Always 0 (Success)
+ * */
+
+int mul(int a, int b)
+{
+	int c;
+	c = a * b;
+	return (c);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,10 @@
 #ifndef My_Header
 #define My_Header
 
+#include <stdio.h>
 int _putchar(char c);
 int main(void);
 int _isupper(int c);
 int _isdigit(int c);
-
+int mul(int a, int b);
 #endif
