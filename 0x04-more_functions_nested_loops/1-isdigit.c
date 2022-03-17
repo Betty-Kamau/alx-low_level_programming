b03984c84166dbf3dc637364b6ee00a4acc2727f
LMI-crypto <bettykamau333@gmail.com>
2022-03-17 18:57:11 +0300

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - checks for digits between 0 and 9
+ * @1 - digit between 0 and 9
+ * @0 - digit not between 0 and 9
+ * Return: int c
+ */
+
+int _isdigit(int c)
+{
+	if(c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
